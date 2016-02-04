6e1834f6d7dd68f0a7a366be45a37bdd43d2b2d6
hoangphanea <hoang.phan@eastagile.com>
2016-02-04 16:35:41 +0700

    [f] Adding singer to exported database

--- lib/workers/dropbox_uploader.rb
+++ lib/workers/dropbox_uploader.rb
@@ -5,15 +5,26 @@ class DropboxUploader
   ACCESS_TOKEN = DBENV['db_key']
 
   def perform(vol, type)
-    # DropboxLink.where(vol: vol).delete_all
     file_name = "vol_#{vol}_#{type.parameterize.underscore}.csv"
     zip_file = "#{file_name}.zip"
 
     CSV.open(file_name, 'wb') do |csv|
-      csv << %w(id name author lyric)
-      Song.where(vol: vol, stype: type).find_each do |song|
-        csv << [song.song_id, song.name, song.singer, song.lyric]
+      csv << %w(id name author singer lyric)
+
+      id_hc_sql = Record.joins(:song).group(:song_id).select('MAX(hit_count)', :song_id).to_sql
+
+      Record.joins(:song).joins(:singer).where('songs.stype = ?', type).where('songs.vol = ?', vol).where("(records.hit_count, records.song_id) IN (#{id_hc_sql})").group('1,2,3,5').pluck('songs.song_id', 'songs.name', 'songs.author', 'array_agg(singers.name)', 'songs.short_lyric').each do |row|
+        csv << [row[0], row[1], row[2], row[3].join(', '), row[4]]
       end
+
+      Song.includes(:records).where(vol: vol, stype: type).where(records: { id: nil }).find_each do |song|
+        csv << [song.song_id, song.name, song.author, '', song.lyric]
+      end
+    end
+
+    if (rows = CSV.read(file_name).count - 1) != (songs_count = Song.where(stype: type, vol: vol).count)
+      p "Song number check failed in vol #{vol} #{type}, #{rows} rows compared to #{songs_count} songs"
+      return
     end
 
     password = SecureRandom.hex
